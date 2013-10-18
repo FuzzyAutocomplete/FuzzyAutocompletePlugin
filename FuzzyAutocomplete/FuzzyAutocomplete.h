fa78968d597f8eeb406cafbc67c4bca1994681c0
Jack Chen (chendo) <github@chen.do>
2013-10-18 20:55:42 +1100

    Initial commit

---
+++ FuzzyAutocomplete/FuzzyAutocomplete.h
@@ -0,0 +1,13 @@
+//
+//  FuzzyAutocomplete.h
+//  FuzzyAutocomplete
+//
+//  Created by Jack Chen on 18/10/2013.
+//  Copyright (c) 2013 chendo interactive. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FuzzyAutocomplete : NSObject
+
+@end
---
+++ FuzzyAutocomplete/FuzzyAutocomplete.m
@@ -0,0 +1,13 @@
+//
+//  FuzzyAutocomplete.m
+//  FuzzyAutocomplete
+//
+//  Created by Jack Chen on 18/10/2013.
+//  Copyright (c) 2013 chendo interactive. All rights reserved.
+//
+
+#import "FuzzyAutocomplete.h"
+
+@implementation FuzzyAutocomplete
+
+@end
