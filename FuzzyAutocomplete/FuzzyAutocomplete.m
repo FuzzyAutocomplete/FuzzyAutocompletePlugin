f74b084c6e655d283f5f6e20c98356211b0027b6
Jack Chen (chendo) <github@chen.do>
2013-10-21 03:12:11 +1100

    Remove testing crap

--- FuzzyAutocomplete/FuzzyAutocomplete.m
+++ FuzzyAutocomplete/FuzzyAutocomplete.m
@@ -12,43 +12,7 @@
 
 + (void)pluginDidLoad:(NSBundle *)plugin
 {
-    DLog(@"Plugin loaded");
-    [self sharedPlugin];
-}
-
-+ (instancetype)sharedPlugin
-{
-    static FuzzyAutocomplete *plugin;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        plugin = [[self alloc] init];
-    });
-    return plugin;
-}
-
-- (instancetype)init
-{
-    if (self = [super init]) {
-//        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(notificationListener:) name:nil object:nil];
-    }
-    return self;
-}
-
-- (void)dealloc
-{
-
-}
-
-- (void)notificationListener:(NSNotification *)notification {
-	// let's filter all the "normal" NSxxx events so that we only
-	// really see the Xcode specific events.
-    if ([[notification name] rangeOfString:@"NS"].location != NSNotFound)
-        return;
-    if ([[notification name] rangeOfString:@"_NS"].location != NSNotFound)
-        return;
-
-    NSLog(@"  Notification: %@", [notification name]);
-
+    NSLog(@"Fuzzy Autocomplete Plugin loaded");
 }
 
 @end
