e8c30a22f81eb439c1cf6496fe6300e7de37adac
Leszek Ślażyński <slazyk@gmail.com>
2014-05-06 11:02:16 +0200

    Declare -[NSArray firstObject] in a category.
    Fixes #42

---
+++ FuzzyAutocomplete/NSArray+FirstObject.h
@@ -0,0 +1,15 @@
+//
+//  NSArray+FirstObject.h
+//  FuzzyAutocomplete
+//
+//  Created by Leszek Ślażyński on 06/05/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (FirstObject)
+
+- (id)firstObject NS_AVAILABLE(10_6, 4_0);
+
+@end
